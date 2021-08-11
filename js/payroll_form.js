6ade557aea42bc3d7ece7e64913313ff0d64399e
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-11 22:38:28 +0530

    Added functionality to add the employee payroll data into local storage

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -25,10 +25,23 @@ const save = (event) => {
     try {
 
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     }
     catch (e) {
         return;
     }
+}
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+    if (employeePayrollList != undefined) {
+        employeePayrollList.push(employeePayrollData);
+    } else {
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
 // Create emppayroll data for adding to the table 
